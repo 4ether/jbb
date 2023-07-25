fde87e302e1d4d5c3a3127ca3403203e24ccb051
Abhirama Aji Mahardhika <97981320+4ether@users.noreply.github.com>
2023-07-26 00:24:22 +0700

    Update hapusAkun.php

--- admins/hapusAkun.php
+++ admins/hapusAkun.php
@@ -5,9 +5,7 @@ session_start();
 $idP = $_GET['id'];
 
 $sqlDel = "DELETE FROM pengguna WHERE id=$idP";
-$sqlDel2 = "DELETE FROM penawaran WHERE id_penawar=$idP";
 $sqlRun = mysqli_query($conn, $sqlDel);
-$sqlRun2 = mysqli_query($conn, $sqlDel2);
 
 if ($sqlRun) {
     header("Location: kelolaUser.php");
